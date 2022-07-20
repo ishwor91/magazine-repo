84e3f85828141479953f5709d7e42b9b7b409261
ishwor91 <ishwor.codewing@gmail.com>
2022-07-20 15:26:11 +0545

    magazine project completed with fully responsive

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 let menu = document.querySelector(".navbar-menu");
 menu.addEventListener('click', hamburger);
-let navBar = document.querySelector(".navbar");
+let navBar = document.querySelector(".header");
 navBar.addEventListener('click', menulist);
 
 function hamburger() {
     menu.classList.toggle('active');
-}
-function menulist() {
     navBar.classList.toggle('active');
+
+}
+
+
+function menulist() {
 }
