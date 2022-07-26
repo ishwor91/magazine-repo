27645aa23a809240fb434027f48fed71c564fb3f
ishwor91 <ishwor.codewing@gmail.com>
2022-07-26 12:03:09 +0545

    magazine completed

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let menu = document.querySelector(".navbar-menu");
-menu.addEventListener('click', hamburger);
+menu.addEventListener("click", hamburger);
 let navBar = document.querySelector(".header");
-navBar.addEventListener('click', menulist);
+navBar.addEventListener("click", menulist);
 
 function hamburger() {
-    menu.classList.toggle('active');
-    navBar.classList.toggle('active');
-
+  menu.classList.toggle("active");
+  navBar.classList.toggle("active");
 }
 
+function menulist() { };
 
-function menulist() {
-}
+
+
